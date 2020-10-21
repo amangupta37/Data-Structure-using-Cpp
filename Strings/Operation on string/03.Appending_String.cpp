71ee4bf62174d6dda422f4d623be2c47168137ba
Aman Gupta <amangupta6121998@gmail.com>
2020-10-21 08:26:38 +0530

    Update 03.Appending_String.cpp

--- Strings/Operation on string/03.Appending_String.cpp
+++ Strings/Operation on string/03.Appending_String.cpp
@@ -2,10 +2,35 @@
 using namespace std;
 int main()
 {
+  
+char str1[100];
+char str2[100];
 
+int i,j,k,str1_length ;
+cin.get(str1,100);
 
-// DISADVANTAGE IS WE CAN NOT RESTICT THE LENGHT OF INPUT STRING
+cin.get();
 
+cin.get(str2,50);
+
+str1_length = strlen(str1);
+
+i =str1_length;
+
+j=0;
+
+while(str2[j]!='\0')
+{
+    str1[i] = str2[j];
+    i++;
+    j++;
+
+}
+str1[i] ='\0';
+cout<<str1;
+
+/* 
+--------------------METHOD 2 -  APPEND BY USING APPEND () METHOD IN C++--------
 
 string strobject; // declaring a  string object
 string newstr;
@@ -19,5 +44,6 @@ cout<<"String after APPEND\n";
 strobject.append(newstr); // append method in c++ for joining two string
 cout<<strobject;
 
+*/
 return 0;
 }
