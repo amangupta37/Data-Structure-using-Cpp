69d9148a6c7d0b4847652ab31fe1e1e93ef6d6cd
Aman Gupta <amangupta6121998@gmail.com>
2020-10-25 18:12:30 +0530

    Update 13.Check_Duplicate.cpp

--- Strings/Operation on string/13.Check_Duplicate.cpp
+++ Strings/Operation on string/13.Check_Duplicate.cpp
@@ -6,9 +6,34 @@ int main()
 {
   char str[100];
 
+  int i=0,count=0,k,length,j;
+
+
   cin.get(str,100);
 
-  
+  length = strlen(str);
+
+//  cout<<length;
+
+cout<<"\nDuplicate Elements are\n";
+
+  for(i=0;i<length-1;i++)
+  {
+       for(j=i+1;j<length;j++)
+       {
+          if(str[i]==str[j] && str[j]!=-1)
+          {
+              cout<<str[j]<<"\n";
+              count++;
+              str[j]=-1;
+          }
+       }
+  }
+
+     cout<<"\nTotal Duplicate Elements are "<<count;
+
+
+
 
 
     return 0;
